1a978f14654926e43ae2a904d04150d678d0422e
Hakan Sander <hakansander@gmail.com>
2017-11-11 15:28:47 +0300

    Functionality for insulin button is added

--- app/src/main/java/halmob/healthhub/DiabActivity.java
+++ app/src/main/java/halmob/healthhub/DiabActivity.java
@@ -1,13 +1,31 @@
 package halmob.healthhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DiabActivity extends AppCompatActivity {
+    private ImageButton insulinButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diab);
+
+        insulinButton = findViewById(R.id.insulin_button);
+        insulinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DiabActivity.this, InsulinActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
