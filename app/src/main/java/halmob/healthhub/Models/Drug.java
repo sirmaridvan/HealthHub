22c57dc46658aab204d549e3c095f3236e65a9e0
Görkem Toppeker <gtoppeker@gmail.com>
2017-11-22 22:44:02 +0300

    Revert "Time List is added to Drug class to hold time notifications."
    This reverts commit 518774d8fd7f6c53a27d90907f16cde44108f82c.

--- app/src/main/java/halmob/healthhub/Models/Drug.java
+++ app/src/main/java/halmob/healthhub/Models/Drug.java
@@ -1,23 +1,15 @@
 package halmob.healthhub.Models;
 
-import com.google.firebase.database.IgnoreExtraProperties;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by RIDVAN SIRMA on 11/10/2017.
  */
 
-@IgnoreExtraProperties
 public class Drug {
     String nameMedicine;
     String howMany;
     String startDate;
     String endDate;
     String time;
-    // String []times;
-    List <String> timeList = new ArrayList<>();
 
     public String getName() { return nameMedicine;}
     public void setName(String nameMedicine) { this.nameMedicine = nameMedicine; }
@@ -45,12 +37,5 @@ public class Drug {
 
     public String getTime() { return time; }
     public void setTime(String time) { this.time = time; }
-
-    public List<String> getTimeList() { return timeList; };
-    public void setTimeList(List<String> timeList){ this.timeList = timeList; };
-
-    // public String[] getTimes(){ return times; };
-    // public void setTimes(String[] times){ this.times = times; };
-
 
 }
