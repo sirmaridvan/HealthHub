518774d8fd7f6c53a27d90907f16cde44108f82c
Görkem Toppeker <gtoppeker@gmail.com>
2017-11-22 22:36:10 +0300

    Time List is added to Drug class to hold time notifications.

--- app/src/main/java/halmob/healthhub/Models/Drug.java
+++ app/src/main/java/halmob/healthhub/Models/Drug.java
@@ -1,15 +1,23 @@
 package halmob.healthhub.Models;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by RIDVAN SIRMA on 11/10/2017.
  */
 
+@IgnoreExtraProperties
 public class Drug {
     String nameMedicine;
     String howMany;
     String startDate;
     String endDate;
     String time;
+    // String []times;
+    List <String> timeList = new ArrayList<>();
 
     public String getName() { return nameMedicine;}
     public void setName(String nameMedicine) { this.nameMedicine = nameMedicine; }
@@ -37,5 +45,12 @@ public class Drug {
 
     public String getTime() { return time; }
     public void setTime(String time) { this.time = time; }
+
+    public List<String> getTimeList() { return timeList; };
+    public void setTimeList(List<String> timeList){ this.timeList = timeList; };
+
+    // public String[] getTimes(){ return times; };
+    // public void setTimes(String[] times){ this.times = times; };
+
 
 }
